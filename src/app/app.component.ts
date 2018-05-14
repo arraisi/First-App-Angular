cde2617b13957f9027ee2b5ecdd5f77aeebbd64b
arraisi <itsarraisi@gmail.com>
2018-05-14 16:43:53 +0700

    Make form with ANGULAR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'app';
+  employee: Employees[] = [];
+
+  employees: Employees = new Employees();
+  
+  kirimData(employees){
+    console.log(this.employees);
+    if(this.employees.id && this.employees.nama){
+      this.employee.push(this.employees);
+    }
+    this.employees = new Employees();
+  }
+  
+}
+export class Employees{
+  constructor(public id?: number, public nama?: string){}
 }
